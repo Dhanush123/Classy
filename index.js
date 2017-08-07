c37c3819fc27153f9d45157eb6cd44ec7078f53e
Dhanush Patel <dhanush.patel@ymail.com>
2017-08-06 17:56:26 -0700

    better var naming

--- index.js
+++ index.js
@@ -3,15 +3,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const restService = express();
+const server = express();
 
-restService.use(bodyParser.urlencoded({
+server.use(bodyParser.urlencoded({
     extended: true
 }));
 
-restService.use(bodyParser.json());
+server.use(bodyParser.json());
 
-restService.post('/echo', function(req, res) {
+server.post('/hook', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     return res.json({
         speech: speech,
@@ -21,6 +21,6 @@ restService.post('/echo', function(req, res) {
 });
 
 
-restService.listen((process.env.PORT || 8000), function() {
+server.listen((process.env.PORT || 8000), function() {
     console.log("Server up and listening");
 });
